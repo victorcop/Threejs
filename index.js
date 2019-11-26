dd55b4fcfba439569858b6dc5c7981ac1489db69
Victor Velasquez <victorcop90@gmail.com>
2019-11-25 22:20:17 -0500

    POC

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-((space3, undefined) => {
+/*((space3, undefined) => {
     space3.scene = new THREE.Scene();
     space3.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     space3.renderer = new THREE.WebGLRenderer();
+
+
 
     // Define threejs
     space3.threejs = function () {
@@ -21,6 +23,11 @@
         init: () => {
             space3.renderer.setSize(window.innerWidth, window.innerHeight);
             document.body.appendChild(space3.renderer.domElement);
+            let geometry = new THREE.CylinderBufferGeometry(5, 5, 20, 32);
+            let material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+            let cylinder = new THREE.Mesh(geometry, material);
+            space3.scene.add(cylinder);
+            space3.camera.position.z = 10;
         },
 
         /// <summary>Game Logic</summary>
@@ -58,4 +65,31 @@
 window.onload = () => {
     space3.threejs = new space3.threejs();
 };
+*/
 
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+// create the shape
+var geometry = new THREE.CylinderBufferGeometry( .2, .2, 20, 32 );
+// create a material, colour or image texture
+var material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 50;
+// game logic
+var update = function () {
+};
+// draw scene
+var render = function () {
+    renderer.render(scene, camera);
+};
+// run game loop (update, render, repeat)
+var GameLoop = function () {
+    requestAnimationFrame(GameLoop);
+    update();
+    render();
+};
+GameLoop();
